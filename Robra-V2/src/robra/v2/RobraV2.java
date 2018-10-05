8dd1e7ec5bda09a1ead9000069b2edf3eaa37665
paulitafarias <paula.farias96@gmail.com>
2018-10-05 10:29:01 -0300

    Inclui libreria opencsv y la dependencia common lang, en el main estan las dos funciones basicas y en el repos tmb estan los archivos para probarlas

--- Robra-V2/src/robra/v2/RobraV2.java
+++ Robra-V2/src/robra/v2/RobraV2.java
@@ -5,6 +5,17 @@
  */
 package robra.v2;
 
+import com.opencsv.*;
+import java.io.FileReader;
+//import java.io.FileWriter;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.awt.EventQueue;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.io.*;
+//import javax.swing.*;
+
 /**
  *
  * @author Paulita
@@ -15,7 +26,42 @@ public class RobraV2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        try {
+        String archCSV = "C:\\Repos\\Robra-V2\\Robra-V2\\Archivos-testing\\ISO-Codes.csv";
+        
+//        Leer de a una las filas del csv
+        try (CSVReader csvReader = new CSVReader(new FileReader(archCSV))) {
+            String[] fila = null;
+            while((fila = csvReader.readNext()) != null) {
+                System.out.println(fila[0]
+                        + " | " + fila[1]
+                        + " |  " + fila[2]);
+            }   
+        }
+//        Leer y obtener todas las filas juntas
+//        CSVReader csvReader = new CSVReader(new FileReader(archCSV));
+//        List<String[]> datos = csvReader.readAll();
+        }
+        catch (Exception ex){
+        System.out.println(ex);
+        }
+        
+//        Exporta archivo
+//        try {
+//            String [] pais = {"Spain", "ES", "ESP", "724", "Yes"};
+//            String archCSV = "C:\\Repos\\Robra-V2\\Robra-V2\\Archivos-testing\\Para-crear.csv";
+//            
+//            CSVWriter writer = new CSVWriter(new FileWriter(archCSV));
+//
+//            writer.writeNext(pais);
+//
+//            writer.close();
+//        }
+//        catch (Exception ex){
+//            System.out.println(ex);
+//        }
+      
     }
     
 }
